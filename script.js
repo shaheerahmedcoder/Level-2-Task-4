193007fc8ede57450eb4123eaa9c72980201a8ae
Shaheer Ahmed <shaheerahmed.0206@gmail.com>
2026-01-22 22:08:49 +0500

    Update script.js

--- script.js
+++ script.js
@@ -63,10 +63,7 @@
       currentPage = page;
       displayPosts(page);
     }
-    function goToPage(page) {
-      currentPage = page;
-      displayPosts(page);
-    }
+
 
     function filterPosts(category) {
       document.querySelectorAll('.filter button').forEach(btn => btn.classList.remove('active'));
