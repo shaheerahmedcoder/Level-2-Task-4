a69e777b33d594e4a26f2b8a703bac175ec2c936
Shaheer Ahmed <shaheerahmed.0206@gmail.com>
2026-01-23 13:52:07 +0500

    Remove duplicate variable declarations
    Removed redundant variable declarations for postsPerPage and currentPage.

--- script.js
+++ script.js
@@ -17,11 +17,6 @@
     let currentPage = 1;
     let filteredPosts = posts;
 
-    const postsPerPage = 4;
-    let currentPage = 1;
-
-
-
     function displayPosts(page) {
       const start = (page - 1) * postsPerPage;
       const end = start + postsPerPage;
