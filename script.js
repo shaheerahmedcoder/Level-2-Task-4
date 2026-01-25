c9e224da86b9c0be5e8d8f7487343314dcd67cbd
Shaheer Ahmed <shaheerahmed.0206@gmail.com>
2026-01-25 23:06:10 +0500

    Update script.js

--- script.js
+++ script.js
@@ -17,6 +17,22 @@
     let currentPage = 1;
     let filteredPosts = posts;
 
+    const postsPerPage = 4;
+    let currentPage = 1;
+    let filteredPosts = posts;
+
+    const postsPerPage = 4;
+    let currentPage = 1;
+    let filteredPosts = posts;
+
+    const postsPerPage = 4;
+    let currentPage = 1;
+    let filteredPosts = posts;
+
+    const postsPerPage = 4;
+    let currentPage = 1;
+    let filteredPosts = posts;
+
 
     function displayPosts(page) {
       const start = (page - 1) * postsPerPage;
